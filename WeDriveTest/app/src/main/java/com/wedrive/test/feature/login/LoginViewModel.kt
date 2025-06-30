e69e94bc13061b4114ec6b0f1ecf3e99c97175f2
SJ <mh050210@namuplanet.com>
2025-06-30 22:58:49 +0900

    [Chore] 코드 정리

--- WeDriveTest/app/src/main/java/com/wedrive/test/feature/login/LoginViewModel.kt
+++ WeDriveTest/app/src/main/java/com/wedrive/test/feature/login/LoginViewModel.kt
@@ -8,23 +8,16 @@ import com.wedrive.test.WeDriveTestApplication
 import com.wedrive.test.api.interceptor.DlApiHeader
 import com.wedrive.test.api.service.AuthService
 import com.wedrive.test.extension.getMessage
-import timber.log.Timber
 
 class LoginViewModel : BaseViewModel() {
-
     private val authService = AuthService.authService
 
     val moveToHome = SingleLiveEvent<Void?>()
 
     fun login(mid: String, pwd: String) {
         executeApi(
-            apiCall = {
-                authService.getAuthToken(LoginRequest(mid, pwd))
-            },
+            apiCall   = { authService.getAuthToken(LoginRequest(mid, pwd)) },
             onSuccess = {
-                Timber.d("access token  : ${it.accessToken}")
-                Timber.d("refresh token : ${it.refreshToken}")
-
                 WeDriveTestApplication.instance.pref[DlApiHeader.AUTH_TOKEN]    = "Bearer ${it.accessToken}"
                 WeDriveTestApplication.instance.pref[DlApiHeader.REFRESH_TOKEN] = "${it.refreshToken}"
 
